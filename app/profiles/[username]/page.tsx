c9673c5b2cc685b7fd292712550d5e921fabb87a
Abdulrehman01dev <hunaindev1@gmail.com>
2025-07-16 09:59:29 +0500

    debug

--- app/profiles/[username]/page.tsx
+++ app/profiles/[username]/page.tsx
@@ -102,7 +102,7 @@ const ProfilePage = () => {
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [postToDelete, setPostToDelete] = useState<number | null>(null);
   const [modalImage, setModalImage] = useState<string | null>(null);
-  const [selectedPost, setSelectedPost] = useState<Post | any>(null);
+  const [selectedPost, setSelectedPost] = useState(null);
   const [modalComments, setModalComments] = useState<ModalComment[]>([]);
 
   useEffect(() => {
